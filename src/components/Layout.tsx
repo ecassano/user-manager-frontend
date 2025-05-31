22ba481d5386ce0f269ae4699efc6f1641029232
eduardosa54 <eduardo.sa@rio.rj.gov.br>
2025-05-30 22:44:18 -0300

    feat: Add logo to Layout header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom"
+import logo from "../assets/logo-conectar.svg"
+
 export const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100 dark:dark-background">
-      <header className="bg-gray-200 text-center p-2 text-sm text-gray-500">
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <header className="flex items-center justify-center bg-muted-700 shadow-2xs text-center p-2 text-sm text-gray-500">
+        <img src={logo} alt="logo" className="w-36 h-18" />
       </header>
       <main>
         <Outlet />
